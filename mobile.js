f8a750d271f3f54d4ca9c7a5f11e752e0f5adfe7
TonyITman <119666149+TonyITman@users.noreply.github.com>
2023-07-21 17:53:48 +0200

    Add files via upload

---
+++ mobile.js
@@ -0,0 +1,9 @@
+
+    function openmenu() {
+        document.getElementById("nav-link").style.right ="0";
+    }
+
+    function closemenu() {
+        document.getElementById("nav-link").style.right = "-200px";
+        document.getElementById("fa-solid").style.display ="none";
+    }
